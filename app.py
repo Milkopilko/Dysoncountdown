f9151fa8e67ffd3058e8c1d3a07fa6a3126eee6d
Milkopilko <milkopilko@hotmail.com>
2022-01-16 21:01:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify, make_response, request
 import dysontime
 
 app = Flask(__name__)
@@ -10,6 +10,12 @@ def homepage():
     countdown = dysontime.dysontimefunction()
     print(countdown)
     return render_template('homepage.html', countdown=countdown)
+
+@app.route('/getcountdown', methods=["POST"])
+def getcountdown():
+    countdown = dysontime.dysontimefunction()
+    res = make_response(jsonify({"message":countdown}), 200)
+    return res
 
 if __name__ == '__main__':
     app.run(host="192.168.1.108", port=8080, debug=True)
